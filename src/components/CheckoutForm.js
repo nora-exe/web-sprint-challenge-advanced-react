0d9e6541c9f727a2ac43a5dbbe031f7fc1cea01f
Nora C <nocorser@gmail.com>
2021-03-12 18:44:12 -0600

    converted stateful logic

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import useForm from "../hooks/useForm";
 
 const initialValue = {
   firstName: "",
@@ -14,12 +15,12 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [values, showSuccessMessage, setShowSuccessMessage, handleChanges] = useForm(initialValue, false);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,13 @@
-// write your custom hook here to control your checkout form
+// Custom hook to control checkout form
+import {useState} from "react";
+
+const useForm = (initialValue, initialSuccessValue) => {
+    const [values, setValues] = useState(initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(initialSuccessValue);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+    return [values, showSuccessMessage, setShowSuccessMessage, handleChanges];
+  };
+export default useForm;
